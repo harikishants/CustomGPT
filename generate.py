a0fb718cb9322a1b9b250d6d9619ce569ac68d1e
harikishants <harikishants9899@gmail.com>
2025-06-18 19:02:52 +0530

    deleted print statements

--- generate.py
+++ generate.py
@@ -16,9 +16,6 @@ if __name__ == "__main__":
     df = pd.read_parquet('data/Wikitext-2_raw/test-00000-of-00001.parquet')
 
     text = ''.join(df['text'].dropna())
-    print(text[:1000])
-    exit()
-
     model, optimizer, epoch, val_loss, hyperparams = load_gpt(CustomGPT, torch.optim.AdamW, file_path='checkpoints/gpt_model_Wiki_best.pt', device='cuda')
     prompt = "Robert Boulter is an English film , television and theatre actor . He had a guest starring role on the television series The Bill in 2000 . This was followed by a starring role in the play Herons written by Simon Stephens , which was performed in 2001 at the Royal Court Theatre ."
 
